01fec4dbd28f4b2ff7393036d7a00a0e83ade09d
Onyedikachi Eze <thekacchi1@gmail.com>
2024-07-16 01:17:11 +0000

    code for home page(index.html) and blog page (blog.html) invluding script hat opens the test page

---
+++ script.js
@@ -0,0 +1,9 @@
+function redirectToTestPage() {
+    // Add logic to redirect users to the test.html page
+    window.location.href = 'test.html';
+}
+
+function redirectToTestPage2() {
+    // Add logic to redirect users to the test.html page
+    window.location.href = 'test2.html';
+}
